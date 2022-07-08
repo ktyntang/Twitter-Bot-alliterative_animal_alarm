6c46af82c434cf18d063506c07baa06abf5b0acb
ktyntang <ktyntang@hotmail.com>
2022-07-08 19:34:25 +0100

    pylinted

--- alliterative_animal_alarm.py
+++ alliterative_animal_alarm.py
@@ -1,7 +1,7 @@
-import tweepy
 from string import ascii_lowercase
 from random import choice
 from wonderwords import RandomWord
+import tweepy
 # from icrawler.builtin import GoogleImageCrawler
 
 def alliterative_content():
@@ -9,26 +9,26 @@ def alliterative_content():
     letter = choice(ascii_lowercase)
 
     with open('animals.txt','r+') as myfile:
-        list = myfile.read().splitlines()
-        prev_letter = list[0]
-        animals = list[1:]        
+        lines = myfile.read().splitlines()
+        prev_letter = lines[0]
+        animals = lines[1:]
 
-        if letter == prev_letter:
+        while letter == prev_letter:
             letter = choice(ascii_lowercase)
 
         myfile.seek(0)
         myfile.write(letter)
-            
+
     animal_generator = RandomWord(animal=animals)
 
-    w= RandomWord()
-    adjective = w.word(starts_with = letter, include_categories=["adjective"])
-    verb_1 = w.word(starts_with = letter, include_categories=["verb"])
-    verb_2 = w.word(starts_with = letter, include_categories=["verb"])
+    rw= RandomWord()
+    adjective = rw.word(starts_with = letter, include_categories=["adjective"])
+    verb_1 = rw.word(starts_with = letter, include_categories=["verb"])
+    verb_2 = rw.word(starts_with = letter, include_categories=["verb"])
     animal = animal_generator.word(starts_with = letter)
 
-    if verb_1 == verb_2:
-        verb_2 = w.word(starts_with = letter, include_categories=["verb"])
+    while verb_1 == verb_2:
+        verb_2 = rw.word(starts_with = letter, include_categories=["verb"])
 
     content = (
         f'''tick tock! it's {letter.upper()} o'clock!
@@ -36,12 +36,19 @@ def alliterative_content():
         )
 
     #IMAGE when Twitter V2 API allows it
-    # google_Crawler = GoogleImageCrawler(storage = {'root_dir': r'C:\Users\ktynt\Desktop\Py_Projects\twitter_bot'})
+    # google_Crawler = GoogleImageCrawler(
+    # storage = {'root_dir': r'C:\Users\ktynt\Desktop\Py_Projects\twitter_bot'}
+    # )
     # google_Crawler.crawl(keyword = f'{adjective} {animal}', max_num = 1)
     return content
 
 def post_tweet(content):
-    client = tweepy.Client(consumer_key='RGqpj7mID6idhtpklXO219eab', consumer_secret='5sTRD02qtLmAbhruVEnLDny5LegOL3ykRwPevD8Hocx4d0nTh5', access_token='1465738470068871170-1aodaw0786b5a8uDRH1q3ISUzcz0bO', access_token_secret='ddzYusNniUX4oy4PZ1sff25Q6NyPQY2ZdtF1DKkt9Q3Jj')
+    client = tweepy.Client(
+        consumer_key='RGqpj7mID6idhtpklXO219eab',
+        consumer_secret='5sTRD02qtLmAbhruVEnLDny5LegOL3ykRwPevD8Hocx4d0nTh5',
+        access_token='1465738470068871170-1aodaw0786b5a8uDRH1q3ISUzcz0bO',
+        access_token_secret='ddzYusNniUX4oy4PZ1sff25Q6NyPQY2ZdtF1DKkt9Q3Jj'
+        )
     client.create_tweet(text = content)
 
 def main():
@@ -50,5 +57,3 @@ def main():
 if __name__ == '__main__':
     main()
     print('tweet tweet!')
-
-
